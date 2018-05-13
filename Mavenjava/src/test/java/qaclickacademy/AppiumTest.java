68f9cab77c6250492cd6216727a565ec57a853fd
RAHMAN <RAHMAN@192.168.1.10>
2018-05-13 19:53:13 -0400

    Created Automation Project

---
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+	
+	
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,29 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+
+public class RestAPITest {
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+	@AfterTest
+	public void Mamu()
+	{
+		System.out.println("Mamu ");
+	}
+	
+}
